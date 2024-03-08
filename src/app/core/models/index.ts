af03b72ccd7123727e7d4db3d963bc586ed47b6d
alejandrolink <alejandro@linkapital.com.br>
2024-03-08 09:30:29 -0300

    ci docker

--- src/app/core/models/index.ts
+++ src/app/core/models/index.ts
@@ -2,7 +2,7 @@ import {Options} from './utils';
 import {WsMessage} from './notify';
 import {UnitsMeasurementsDTO} from './units-measurements';
 import {CategoryDto} from './category';
-import {User,BankAccountTO,GeneratePassword,UpdatePassword} from './User';
+import {User,BankAccountTO,GeneratePassword,UpdatePassword} from './user';
 import {AuthMap,DomainDetailTO,AuthorityTO,Role} from './role';
 
 export {
