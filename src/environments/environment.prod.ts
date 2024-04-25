63881db88082f2563c23a6cc4418caab3dfef9e8
alejandrolink <alejandro@linkapital.com.br>
2024-04-25 08:49:10 -0300

    many changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ export const environment = {
     timeChecks: 10 * 60 * 1000,
     websocket: 'http://127.0.0.1:8020/api/websocket',
     privateKey: "Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E=",
-    apiURL: 'http://ec2-34-204-94-88.compute-1.amazonaws.com/api/',
+    apiURL: 'http://ec2-34-204-94-88.compute-1.amazonaws.com/api',
 };
