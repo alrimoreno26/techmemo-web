ea2d3c9e00cfaecb271e64d6ed518c74cf5fafbd
alejandrolink <alejandro@linkapital.com.br>
2024-05-01 19:56:53 -0300

    enviroments

--- src/app/modules/shops/market/components/home/section/banner/banner.component.ts
+++ src/app/modules/shops/market/components/home/section/banner/banner.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'banner',
   templateUrl: './banner.component.html',
-  styleUrl: './banner.modals.scss'
+  // styleUrl: './banner.modals.scss'
 })
 export class BannerComponent {
 
