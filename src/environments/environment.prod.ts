1e5daeeb8070884fe6511e4bd68b89e97f153595
alejandrolink <alejandro@linkapital.com.br>
2024-09-02 09:56:36 -0300

    arreglos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
     timeChecks: 10 * 60 * 1000,
-    websocket: 'http://ec2-34-204-94-88.compute-1.amazonaws.com/api',
+    websocket: 'http://ec2-34-204-94-88.compute-1.amazonaws.com/api/v1/websocket',
     privateKey: "Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E=",
     apiURL: 'http://ec2-34-204-94-88.compute-1.amazonaws.com/api',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     timeChecks: 5 * 60 * 1000,
-    websocket: 'http://ec2-34-204-94-88.compute-1.amazonaws.com/api',
+    websocket: 'http://ec2-34-204-94-88.compute-1.amazonaws.com/api/v1/websocket',
     privateKey: "Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E=",
     apiURL: 'http://ec2-34-204-94-88.compute-1.amazonaws.com/api',
 };
