6db91f1aa75693b2936f167c72c986243ef685f6
alejandrolink <alejandro@linkapital.com.br>
2025-02-23 13:29:44 -0300

    arreglos

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: true,
-    timeChecks: 10 * 60 * 1000,
-    websocket: 'http://ec2-34-204-94-88.compute-1.amazonaws.com/api/v1/websocket',
+    timeChecks: 5 * 60 * 1000,
+    websocket: 'http://127.0.0.1:8020/api/websocket',
     privateKey: "Yn2kjibddFAWtnPJ2AFlL8WXmohJMCvigQggaEypa5E=",
-    apiURL: 'http://ec2-34-204-94-88.compute-1.amazonaws.com/api',
+    apiURL: 'http://localhost:5000/api',
 };
